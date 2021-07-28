15b43bd132c816c745d3814ba0f0f839b8fd250a
minkyoungyeon <726aa@naver.com>
2021-07-28 19:57:56 +0900

    first commit

--- applyproject/settings.py
+++ applyproject/settings.py
@@ -131,3 +131,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
